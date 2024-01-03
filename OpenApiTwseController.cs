77d11f590046c1176fd547b4af7afec65354017c
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-01-03 08:37:45 +0800

    Update OpenApiTwseController.cs

--- OpenApiTwseController.cs
+++ OpenApiTwseController.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Configuration;
 using Newtonsoft.Json.Linq;
 using System.IO;
@@ -15,13 +15,15 @@ using System.Threading.Tasks;
 namespace ShioajiBackend.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")] //OpenApiTwse
-    public class OpenApiTwseController : ControllerBase
+
+    [Route("api/[controller]")]
+    public class MI_INDEX20Controller : ControllerBase
     {
         private async Task<Dictionary<string, Dictionary<string, object>>> OpenApiTwseAsync()
         {
             var client = new HttpClient();
-            var response = await client.GetAsync("https://openapi.twse.com.tw/v1/" + "/exchangeReport/MI_INDEX20");
+            var response = await client.GetAsync("https://openapi.twse.com.tw/v1/" +
+                "exchangeReport/MI_INDEX20");
             var json = await response.Content.ReadAsStringAsync();
             List<dynamic> src = JsonConvert.DeserializeObject<List<dynamic>>(json);
 
@@ -32,15 +34,13 @@ namespace ShioajiBackend.Controllers
                 {
                     { "Name", item["Name"].ToString()},
                     { "ClosingPrice", Convert.ToDouble(item["ClosingPrice"])},
-                    { "Change", Convert.ToString(item["Change"])}
+                    { "Change", item["Change"] == "" ? 0 : (Convert.ToDouble(item["Change"]))} // 平盤好像是空值 ☛ " "，故不能用double
                 };
             }
-            return src_new;
+            return (Dictionary<string, Dictionary<string, object>>)src_new;
         }
 
-
-
-        [HttpGet("GetTwseData")]
+        [HttpGet("GetTwseData")] //API尾段URL可在這寫得更易讀
         public async Task<IActionResult> GetTwseData()
         {
             try
@@ -56,6 +56,48 @@ namespace ShioajiBackend.Controllers
     }
 
 
+
+
+    [Route("api/[controller]")]
+    public class FMTQIKController : ControllerBase
+    {
+        private async Task<Dictionary<string, Dictionary<string, object>>> FMTQIKAsync()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://openapi.twse.com.tw/v1/" +
+                "exchangeReport/FMTQIK");
+            var json = await response.Content.ReadAsStringAsync();
+            List<dynamic> src = JsonConvert.DeserializeObject<List<dynamic>>(json);
+
+            Dictionary<string, Dictionary<string, object>> src_new = new Dictionary<string, Dictionary<string, object>>();
+            foreach (var item in src)
+            {
+                src_new[item["Date"].ToString()] = new Dictionary<string, object>
+                {
+                    { "TradeVolume", (Double)item["TradeVolume"]},
+                    { "TradeValue", (Double)item["TradeValue"]},
+                    { "Transaction", (Double)item["Transaction"]},
+                    { "TAIEX", (Double)item["TAIEX"]},
+                    { "Change", (Double)item["Change"]},
+                };
+            }
+            return (Dictionary<string, Dictionary<string, object>>)src_new;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var result = await FMTQIKAsync();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
 }
 
 
