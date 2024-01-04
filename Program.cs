54be5ddd2d787241cbf740e7447939d9a7649ccc
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-01-04 23:44:07 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,13 +7,7 @@ namespace csSJ
     {
         static void Main(string[] args)
         {
-            var data1 = new namespaceSJ.SJCls();
-            data1.SpreadQuote(0, "TX4", "202308");
-            data1.MyKbars(10);
-            data1.MyScanners();
-            data1.MyListPositions("holding");
-            data1.TXFR1CB();
-            Console.ReadLine();
+
         }
     }
 }
