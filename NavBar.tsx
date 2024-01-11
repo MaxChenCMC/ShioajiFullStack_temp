cc4661f9b52b0f8fb817aa55a0b103e765df2df3
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-01-11 14:37:00 +0800

    Update NavBar.tsx

--- NavBar.tsx
+++ NavBar.tsx
@@ -2,38 +2,46 @@ import React, { useState, useEffect } from 'react'
 import {
     AppBar, Toolbar, Box, Grid, Paper, Typography
 } from '@material-ui/core'
+// import axios from 'axios';
+
+interface Data { Acct: string[]; TAIFEX: string[]; TSE: string[]; OTC: string[]; }
 
 const NavBar = () => {
 
-    const [TbarTseTxfOtc, setTbarTseTxfOtc] = useState<any[]>([])
+    const [TbarTseTxfOtc, setTbarTseTxfOtc] = useState<Data>({ Acct: [], TAIFEX: [], TSE: [], OTC: [] });
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await fetch('http://localhost:57064/api/TbarTseTxfOtc');
-                if (!response.ok) {
-                    throw new Error('Error fetching data');
-                }
+                // const response = await fetch('http://localhost:57064/api/ListAccountsSnapshots_NavBar');
+                const response = await fetch('http://localhost:9033/api/ListAccountsSnapshots_NavBar');
+                if (!response.ok) { throw new Error('Error fetching data'); }
                 const data = await response.json();
                 console.log(data);
-                // setTbarTseTxfOtc(Object.entries(data));
-                setTbarTseTxfOtc(data);
+                setTbarTseTxfOtc(data);      // setTbarTseTxfOtc(Object.entries(data));
             } catch (error) { console.log(error) }
         };
         fetchPosts();
     }, []);
+
+    // const [TbarTseTxfOtc, setTbarTseTxfOtc] = useState<Data>({ Acct: [], TAIFEX: [], TSE: [], OTC: [] });
+    // useEffect(() => {
+    //   axios.get('http://localhost:9033/api/ListAccountsSnapshots_NavBar')
+    //     .then(response => setTbarTseTxfOtc(response.data))
+    //     .catch(error => console.log(error));
+    // }, []);    
 
     return (
         <AppBar position='static' style={{ height: 100, padding: 20 }} >
             <Toolbar>
                 <Grid item container justifyContent="space-between" alignItems="center">
                     <Grid item xs={1}>
-                        <Paper style={{ width: 120, height: 70 }}> 指期：{TbarTseTxfOtc[2]}<br />漲跌：{TbarTseTxfOtc[3]}<br />漲幅：{TbarTseTxfOtc[4]}% </Paper>
+                        <Paper style={{ width: 120, height: 70 }}> 指期：{TbarTseTxfOtc.TAIFEX[0]}<br />漲跌：{TbarTseTxfOtc.TAIFEX[1]}<br />漲幅：{TbarTseTxfOtc.TAIFEX[2]}% </Paper>
                     </Grid>
                     <Grid item xs={1}>
-                        <Paper style={{ width: 120, height: 70 }}> 加權：{TbarTseTxfOtc[5]}<br />漲跌：{TbarTseTxfOtc[6]}<br />漲幅：{TbarTseTxfOtc[7]}% </Paper>
+                        <Paper style={{ width: 120, height: 70 }}> 加權：{TbarTseTxfOtc.TSE[0]}<br />漲跌：{TbarTseTxfOtc.TSE[1]}<br />漲幅：{TbarTseTxfOtc.TSE[2]}% </Paper>
                     </Grid>
                     <Grid item xs={1}>
-                        <Paper style={{ width: 120, height: 70 }}> 櫃買：{TbarTseTxfOtc[8]}<br />漲跌：{TbarTseTxfOtc[9]}<br />漲幅：{TbarTseTxfOtc[10]}% </Paper>
+                        <Paper style={{ width: 120, height: 70 }}> 櫃買：{TbarTseTxfOtc.OTC[0]}<br />漲跌：{TbarTseTxfOtc.OTC[1]}<br />漲幅：{TbarTseTxfOtc.OTC[2]}% </Paper>
                     </Grid>
                     <Grid item xs={5}>
                         <Box display="flex" alignItems="center">
@@ -41,7 +49,7 @@ const NavBar = () => {
                         </Box>
                     </Grid>
                     <Grid item xs={3}>
-                        <Typography variant="h6" align='right' >證：****{TbarTseTxfOtc[0]}<br />期：****{TbarTseTxfOtc[1]}</Typography>
+                        <Typography variant="h6" align='right' >證：****{TbarTseTxfOtc.Acct[0]}<br />期：****{TbarTseTxfOtc.Acct[1]}</Typography>
                         <Typography variant="subtitle2" align='right' noWrap>{Date().toString().slice(0, 10)}</Typography>
                     </Grid>
                 </Grid>
