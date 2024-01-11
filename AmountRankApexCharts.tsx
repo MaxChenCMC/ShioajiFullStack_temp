27e2f91ea0d6795de8f90480fea92b21bdf26322
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-01-11 15:48:05 +0800

    Update AmountRankApexCharts.tsx

--- AmountRankApexCharts.tsx
+++ AmountRankApexCharts.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Chart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
-import {  Typography} from '@material-ui/core'
+import { Typography } from '@material-ui/core'
 
 const AmountRankApexCharts: React.FC = () => {
 
@@ -13,31 +13,21 @@ const AmountRankApexCharts: React.FC = () => {
   const [AmountRank, setAmountRank] = useState<any[]>([])
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('http://localhost:57064/api/AmountRankCharts');
+      const response = await fetch('http://localhost:9033/api/ScannersAmountRank');
       const data = await response.json();
-      setAmountRank(data);
-      // 僅OHLC四個值的話有些K棒會畫錯，非得塞V(就算不是張數而是成交量)才畫對
-      console.log(data);
+      setAmountRank(data); console.log(data);
     };
     fetchPosts();
   }, []);
 
   var AmountSum = Object.values(AmountRank).reduce((accumulator, currentValue) => accumulator + currentValue[currentValue.length - 1], 0);
-
-  const series = [
-    {
-      data: Object.entries(AmountRank).map(([key, value]) => ({ x: key, y: value })),
-    },
-  ];
+  const series = [{ data: Object.entries(AmountRank).map(([key, value]) => ({ x: key, y: value })), },];
 
   return (
     <div>
-      {/* <h1>{AmountSum}億</h1> */}
-      {/* 中文股名要寬一點，若僅股號可再寬些 */}
-      <Typography variant="subtitle2" align='center' color='primary' noWrap >Top20 Trading Amt Sum {AmountSum/10} Billion</Typography>
-      <Chart options={options} series={series} type="candlestick" height={285} />
+      <Typography variant="subtitle2" align='center' color='primary' noWrap >Top 20 Trading Amt Total {AmountSum / 10} Billion</Typography>
+      <Chart options={options} series={series} type="candlestick" />
     </div>
   );
 };
-
 export default AmountRankApexCharts;
