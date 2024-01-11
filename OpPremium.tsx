d304becca6c7096c01e9294675ea340885a8a9d2
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-01-11 15:27:56 +0800

    Update OpPremium.tsx

--- OpPremium.tsx
+++ OpPremium.tsx
@@ -1,42 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import {
-    makeStyles, Grid, Box, Typography,
-    TableContainer, Paper, Table, TableHead,
-    TableBody, TableRow, TableCell,
+    makeStyles, Grid, Box, TableContainer, Paper,
     Card, AppBar, Tab, Toolbar,
+    Typography, TableBody, TableRow, TableCell, Table, TableHead,
 } from '@material-ui/core'
+
+interface _OpPremium { strike: string[]; premium: number[]; }
+interface _Margin { Margin: number[]; ListPositions: string | number[]; }
 
 export const OpPremium = () => {
 
-    const [OpPremium, setOpPremium] = useState<any[]>([])
+    const [OpPremium, setOpPremium] = useState<_OpPremium>({ strike: [], premium: [] });
     useEffect(() => {
         const fetchPosts = async () => {
-            try {
-                const response = await fetch('http://localhost:57064/api/OpPremium');
-                if (!response.ok) {
-                    throw new Error('Error fetching data');
-                }
-                const data = await response.json();
-                console.log(data);
-                setOpPremium(data);
-            } catch (error) { console.log(error) }
+            const response = await fetch('http://localhost:9033/api/Snapshots_OpPremium');  // 57064
+            const data = await response.json();
+            setOpPremium(data);
         };
         fetchPosts();
     }, []);
 
-
-    const [Margin, setMargin] = useState<any[]>([])
+    const [MarginListPositions, setMarginListPositions] = useState<_Margin>({ Margin: [], ListPositions: [] });
     useEffect(() => {
         const fetchPosts = async () => {
-            try {
-                const response = await fetch('http://localhost:57064/api/Margin');
-                if (!response.ok) {
-                    throw new Error('Error fetching data');
-                }
-                const data = await response.json();
-                console.log(data);
-                setMargin(data);
-            } catch (error) { console.log(error) }
+            const response = await fetch('http://localhost:9033/api/MarginListPositions');  // 57064
+            const data = await response.json();
+            setMarginListPositions(data);
         };
         fetchPosts();
     }, []);
@@ -50,32 +39,30 @@ export const OpPremium = () => {
                         <TableCell style={{ textAlign: 'center' }} colSpan={2}>Vertical Spread<br />BCSC</TableCell>
                         <TableCell style={{ textAlign: 'center' }} >Parity +/- Strike</TableCell>
                         <TableCell style={{ textAlign: 'center' }} colSpan={2}>Vertical Spread<br />BPSP</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >庫存<br />權益數、可出金</TableCell>
+                        <TableCell style={{ textAlign: 'center' }} >商品：成本<br />權益數、可出金</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     <TableRow>
-                        <TableCell rowSpan={2} style={{ textAlign: 'center' }} >{OpPremium[2] - OpPremium[3]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{OpPremium[3]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{OpPremium[1]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{OpPremium[4]}</TableCell>
-                        <TableCell rowSpan={2} style={{ textAlign: 'center' }} >{OpPremium[4] - OpPremium[5]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{Margin[3]}、{Margin[4]}</TableCell>
+                        <TableCell rowSpan={2} style={{ textAlign: 'center' }} >{OpPremium.premium[0] - OpPremium.premium[1]}</TableCell>
+                        <TableCell style={{ textAlign: 'center' }} >{OpPremium.premium[1]}</TableCell> {/* SC */}
+                        <TableCell style={{ textAlign: 'center' }} >{OpPremium.strike[1]}</TableCell>  {/* upper strike */}
+                        <TableCell style={{ textAlign: 'center' }} >{OpPremium.premium[2]}</TableCell> {/* BP */}
+                        <TableCell rowSpan={2} style={{ textAlign: 'center' }} >{OpPremium.premium[2] - OpPremium.premium[3]}</TableCell>
+                        <TableCell style={{ textAlign: 'center' }} >
+                            {MarginListPositions.ListPositions[0]}：{MarginListPositions.ListPositions[1]}<br />
+                            {MarginListPositions.ListPositions[2]}：{MarginListPositions.ListPositions[3]}
+                        </TableCell>
                     </TableRow>
                     <TableRow>
-                        <TableCell style={{ textAlign: 'center' }} >{OpPremium[2]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{OpPremium[0]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{OpPremium[5]}</TableCell>
-                        <TableCell style={{ textAlign: 'center' }} >{Margin[0]}、{Margin[1]}</TableCell>
+                        <TableCell style={{ textAlign: 'center' }} >{OpPremium.premium[0]}</TableCell> {/* BC */}
+                        <TableCell style={{ textAlign: 'center' }} >{OpPremium.strike[0]}</TableCell>  {/* lower strike */}
+                        <TableCell style={{ textAlign: 'center' }} >{OpPremium.premium[3]}</TableCell> {/* SP */}
+                        <TableCell style={{ textAlign: 'center' }} >{MarginListPositions.Margin[0]}、{MarginListPositions.Margin[1]}</TableCell>
                     </TableRow>
                 </TableBody>
             </Table>
         </>
-
     )
 }
-
 export default OpPremium
-
-
-
